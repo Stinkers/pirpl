4ae067514289f6903e1185c1ab4f466357bc576e
Stinkers <dws@mrao.cam.ac.uk>
2018-01-13 17:33:02 +0000

    Tidying up

--- upload.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-scp -P 22220 pirjb.tgz naich.net:/home/naich/public_html
-scp -P 22220 pifm naich.net:/home/naich/public_html
-scp -P 22220 pirjb.sh naich.net:/home/naich/public_html
-scp -P 22220 pirjb_webserver.sh naich.net:/home/naich/public_html
-scp -P 22220 pirjb_template.html naich.net:/home/naich/public_html
-scp -P 22220 source/pifm.c naich.net:/home/naich/public_html/source
