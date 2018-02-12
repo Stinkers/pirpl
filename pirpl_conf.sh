8d3e9b864310c5acc839bda793852f88021d76a3
Stinkers <dws@mrao.cam.ac.uk>
2018-02-12 10:17:14 +0000

    Move things around and change instructions

--- pirpl_conf.sh
+++ pirpl_conf.sh
@@ -1,5 +1,4 @@
-# Common config file for pirpl.  All these values can be changed, but you
-# probably only want to do the first 3.
+# Common config file for pirpl.  All these values can be changed, but only the first few are useful to change.
 
 # Change these lines to match your system and preferences
 PIFM_FREQUENCY="107.3" # Transmitter frequency
@@ -9,6 +8,8 @@ PIFM_RTEXT="Add some Wibble to your life" # Radio text, 64 chars
 PIFM_BASE="/home/naich/pirpl" # Directory this was installed in
 PIFM_RESOURCES="$PIFM_BASE/resources"
 SERVER_PORT="8081" # Point your browser to http://your_pi_ip:8080
+# PIFM_BINARY="/home/naich/pi/PiFMRDS-Extra/pi_fm_rds"
+PIFM_BINARY="/home/naich/PiFMRDS-Extra/src/pi_fm_rds" # pifm binary
 
 # Other arguments to pass to SOX, e.g.
 # SOX_ARGS="compand 0.3,0.8 6:-70,-60,-20 -11 -90 0.2"
@@ -16,9 +17,9 @@ SERVER_PORT="8081" # Point your browser to http://your_pi_ip:8080
 SOX_ARGS=""
 SOX_FILTER="vol 1.5 loudness"
 
+# Debugging
 DEBUG="true"
 # DEBUG="false"
-
 set -u
 
 # The rest can be left as defaults unless you want to change them
@@ -46,14 +47,12 @@ mimetypes["skipbackicon"]="image/png"
 resources["stepforwardicon"]="$PIFM_RESOURCES/media-step-forward-4x.png"
 mimetypes["stepforwardicon"]="image/png"
 
-
-# PIFM_BINARY="/home/naich/pi/PiFMRDS-Extra/pi_fm_rds"
-PIFM_BINARY="/home/naich/PiFMRDS-Extra/src/pi_fm_rds" # pifm binary
 PIRPL_SERVER="$PIFM_BASE/pirpl_server.sh"
 WEB_FILE="$PIFM_BASE/pirpl_web.html" # Web page to be output
 WEB_INFO="$PIFM_BASE/pirpl_info.txt" # Info for the web server
 CMD_FILE="$PIFM_BASE/pirpl_cmd.txt" # Command file from web server
 PIRPL_TEMPLATE="$PIFM_RESOURCES/pirpl_template.html" # Template for the web server pa
+STREAM_TEMPLATE="$PIFM_RESOURCES/stream_template.html" # Template for steaming.
 PLAYLIST_CONF="$PIFM_BASE/pirpl_playlist.conf"
 PLAYLIST_TEMPLATE="$PIFM_RESOURCES/pirpl_playlist_template.html"
 PLAYLIST_LOCATION="$PIFM_BASE/playlists"
